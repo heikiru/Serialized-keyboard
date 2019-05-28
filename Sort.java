08980d60c59ab21667c9a5bd6c59bb3c3f20cdfd
Nicole <nicoleschmidt2000@gmail.com>
2019-05-28 19:14:57 -0300

    Adding sort

---
+++ Sort.java
@@ -0,0 +1,20 @@
+package keyboard;
+
+import java.util.Random;
+
+public class Sort {
+	
+	private char[] setas = {'↑','→','←','↓'};
+	private Random random;
+	
+	public char random_char() {
+		random = new Random();
+		int number = random.nextInt(4);
+		System.out.println(setas[number]);
+		return setas[number];
+	}
+	public static void main(String[] args) {
+		Sort s = new Sort();
+		s.random_char();
+	}
+}
