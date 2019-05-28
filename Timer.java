0246e72cb8d5d58033b64e2b059a3bac91546e18
Nicole <45316185+nicoschmdt@users.noreply.github.com>
2019-05-28 18:53:12 -0300

    Update Timer.java

--- Timer.java
+++ Timer.java
@@ -3,7 +3,6 @@ package keyboard;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,62 +17,59 @@ public class Timer extends JFrame implements ActionListener{
 	private JButton tButton;
 	private JLabel label;
 	private JPanel tPanel;
-//	private JMenu menu;
-	
-	
+
 	public Timer() {
 		super(":D");
-		
-		tFrame = new JFrame();
-		tFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		tPanel = new JPanel();
 		tPanel.setLayout(new BorderLayout());
-		
+
+		tFrame = new JFrame();
+		tFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		label = new JLabel("Seconds remaining: 0");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
-		
+
 		tButton = new JButton("Start counter");
-		tButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+		tButton.addActionListener((e) -> {
 
-                    @Override
-                    protected Void doInBackground() throws Exception {
-                        for (int i = 20; i >= 0; i--) {
-                            Thread.sleep(1000);
-                            System.out.println(i);
-                            label.setText("Seconds remainig: " + i);
-                        }
+			SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
 
-                        return null;
-                    }
-                };
-                worker.execute();
-			}
+				@Override
+				protected Void doInBackground() throws Exception {
+					for (int i = 20; i >= 0; i--) {
+						Thread.sleep(1000);
+						System.out.println(i);
+						label.setText("Seconds remainig: " + i);
+					}
+
+					return null;
+				}
+			};
+			worker.execute();
 		});
-		tPanel.add(label, BorderLayout.CENTER);
-		tPanel.add(tButton, BorderLayout.SOUTH);
+
+		tPanel.add(label, BorderLayout.LINE_START);
+		tPanel.add(tButton, BorderLayout.EAST);
 		tFrame.add(tPanel);
 		tFrame.pack();
-		
+
 		tFrame.setVisible(true);
-		
+
 	}
 
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                Timer view = new Timer();
-                
-            }
-        });
+			public void run() {
+				Timer view = new Timer();
+			}
+		});
 	}
 }
