eb2e362324a2fecdf1201ae15fc400acc2785660
gabiramire <45242292+gabiramire@users.noreply.github.com>
2019-05-30 21:40:59 -0300

    Update Deserializer.java

--- Deserializer.java
+++ Deserializer.java
@@ -1,5 +1,53 @@
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class Deserializer {
+	   private ObjectInputStream input;
+	   public void openFile()
+	   {
+	      try {
+	         input = new ObjectInputStream(new FileInputStream("data_input.ser"));
+	      } 
+	      catch(IOException ioException) {
+	         System.err.println("ERROR OPENING FILE.");
+	      } 
+	   } 
 
-}
+	   public void readRecords(){
+		  DataInput dataInput;
+	      System.out.printf("%-10s\n", "Text ");
+
+	      try { 
+	         while (true){
+	        	 dataInput = ( DataInput ) input.readObject();
+	           System.out.printf("%-12s\n", dataInput.getWhatsWritten());
+	         }
+	      }
+	      catch ( EOFException endOfFileException ) {
+	         return; 
+	      } 
+	      catch ( ClassNotFoundException classNotFoundException ) {
+	         System.err.println("UNABLE TO CREATE OBJECT.");
+	      } 
+	      catch ( IOException ioException ) {
+	         System.err.println("ERROR DURING READING FROM FILE.");
+	      } 
+	   } 
+  
+  
+	   public void closeFile(){
+	      try {
+	         if (input != null)
+	            input.close();
+	         System.exit(0);
+	      } // end try
+	      catch ( IOException ioException )
+	      {
+	         System.err.println("ERROR CLOSING FILE.");
+	         System.exit(1);
+	      }
+	   }
+	} 
+
