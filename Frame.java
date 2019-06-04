1133716c3e4532d43d4ede5d4ad32642fb4df3b3
gabiramire <45242292+gabiramire@users.noreply.github.com>
2019-06-03 21:27:08 -0300

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,26 +1,25 @@
-package hey;
-
+package keyboard;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
+import java.awt.event.KeyListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.*;
 
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class Frame extends JFrame{
 
-	private JPanel panelTextArea, panelText, panelTeclas;
+	private JPanel panelTextArea, panelText, panelPangrama;
+	private PanelTeclas panelTeclas;
 	private JTextArea textArea;
 	private JLabel pangramaText, pontuacao;
-	private boolean enterPressed;
 	private JRadioButton[] pangramaButton;
 	private JButton jogoButton;
 	private ButtonGroup pangramaGroup;
@@ -29,10 +28,16 @@ public class Frame extends JFrame{
 	private StringToChar sc;
 	private RadioButtonHandler pangramaButtonHandler;
 	private Pangrama selectedPan;
+	private int points;
 	
 	
 	
-	
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int erros) {
+		this.points=erros;
+	}
 	
 	public String get_Whole_textArea(){
 		String temp = "";
@@ -43,59 +48,22 @@ public class Frame extends JFrame{
 	public Pangrama getSelectedPan() {
 		return selectedPan;
 	}
+	
+	
 
 	public Frame(){
-		super("Aplicativo de Digitação");
+		super("Aplicativo de DigitaÃ§Ã£o");
 		setLayout(new FlowLayout());
 
 		JMenu testePangrama = new JMenu("Teste Pangrama");
 		testePangrama.setMnemonic('T');
-		JMenu historico = new JMenu("Histórico");
-		historico.setMnemonic('H');
-		//
-		JMenuItem feature = new JMenuItem("Jogo");
-		feature.setMnemonic('J');
-		//
+		JMenu historico = new JMenu("HistÃ³rico");
+		testePangrama.setMnemonic('H');
+
 		JMenuBar bar = new JMenuBar();
 		setJMenuBar(bar);
 		bar.add(testePangrama);
 		bar.add(historico);
-		//
-		bar.add(feature);
-		//
-		
-
-		pangramaGroup = new ButtonGroup();
-			      
-	      sc = new StringToChar();
-	      pangrama = new Pangrama[5];
-	      pan = new String[5];
-	      pan[0]= "Um pequeno	jabuti xereta viu dez cegonhas felizes.";
-	      pan[1]= "Quem traz CD, LP, fax, engov e whisky JB?";
-	      pan[2]=	"Gazeta publica hoje breve nota de faxina na quermesse.";
-	      pan[3]= "Jovem craque belga prediz falhas no xote.";
-	      pan[4]= "Bancos fúteis pagavam-lhe queijo, whisky e xadrez.";
-	  	
-	  	for(int i=0; i<pan.length; i++) {
-	  		pangrama[i]= new Pangrama(sc.stringToChar(pan[i]),0);
-	  	}
-	  	
-	  	pangramaButtonHandler = new RadioButtonHandler();
-		pangramaButton = new JRadioButton[pan.length];
-	      
-	      for(int i=0; i<pan.length; i++) {
-	    	  pangramaButton[i] = new JRadioButton(pan[i]);
-	    	  pangramaButton[i].setMaximumSize(new Dimension(95,25));
-	    	  pangramaGroup.add(pangramaButton[i]);
-	    	  pangramaButton[i].addItemListener(pangramaButtonHandler);
-	      }
-		
-		
-		
-		
-		
-		
-		
 		
 		
 		
@@ -107,20 +75,65 @@ public class Frame extends JFrame{
 		
 		
 		//
+		
+		
+		
+
 		panelTextArea = new JPanel( new BorderLayout() );
 		panelTextArea.setPreferredSize(new Dimension(900, 200));
-		panelTextArea.add(new JTextArea(" "), BorderLayout.CENTER);
+		textArea = new JTextArea(" ");
+		textArea.addKeyListener(new keyboardListener());
+		panelTextArea.add(textArea, BorderLayout.CENTER);
 		add(panelTextArea);
 
 		panelText = new JPanel( new FlowLayout(FlowLayout.LEFT) );
-		panelText.setPreferredSize(new Dimension(900, 100));
+		panelText.setPreferredSize(new Dimension(900, 175));
 		panelText.add(Box.createVerticalStrut(150));
+		
+		
+		
+		
+		pangramaGroup = new ButtonGroup();
+	      
+	      sc = new StringToChar();
+	      pangrama = new Pangrama[5];
+	      pan = new String[5];
+	      pan[0]= "Um pequeno jabuti xereta viu dez cegonhas felizes.";
+	      pan[1]= "Quem traz CD, LP, fax, engov e whisky JB?";
+	      pan[2]= "Gazeta publica hoje breve nota de faxina na quermesse.";
+	      pan[3]= "Jovem craque belga prediz falhas no xote.";
+	      pan[4]= "Bancos fúteis pagavam-lhe queijo, whisky e xadrez.";
+	  	
+	  	for(int i=0; i<pan.length; i++) {
+	  		pangrama[i]= new Pangrama(sc.stringToChar(pan[i]),0);
+	  	}
+	  	
+	  	pangramaButtonHandler = new RadioButtonHandler();
+		pangramaButton = new JRadioButton[pan.length];
+		Box box = Box.createVerticalBox();
+
+	
+	      
+	      for(int i=0; i<pan.length; i++) {
+	    	  pangramaButton[i] = new JRadioButton(pan[i]);
+	    	  pangramaButton[i].setFont(new Font(pangramaButton[i].getName(), Font.PLAIN, 18));
+	    	  pangramaGroup.add(pangramaButton[i]);
+	    	  box.add(pangramaButton[i]);
+	    	  pangramaButton[i].addItemListener(pangramaButtonHandler);
+	      }
+		
+		
 		pangramaText = new JLabel("Label para o pangrama");
-		pangramaText.setFont(new Font(pangramaText.getName(), Font.PLAIN, 18));
-		pangramaText.setPreferredSize(new Dimension(450, 20));
-		panelText.add(pangramaText);
-		pontuacao = new JLabel("Pontuação");
+		pangramaText.setFont(new Font(pangramaText.getName(), Font.PLAIN, 16));
+		box.setPreferredSize(new Dimension(600, 250));
+		panelText.add(box);
+		
+		
+		
+		
+		pontuacao = new JLabel("Pontuação: " + this.points);
 		pontuacao.setFont(new Font(pontuacao.getName(), Font.PLAIN, 18));
+		pontuacao.setPreferredSize(new Dimension(150, 50));
 		panelText.add(pontuacao);
 		add(panelText);
 
@@ -130,26 +143,46 @@ public class Frame extends JFrame{
 		add(panelTeclas);
 		
 		
+
 	}
 
-//	private class KeyHandler implements KeyAdapter {
-//        public void keyPressed(KeyEvent e){
-//            if (e.getKeyCode() == 13) // Enter-key
-//                enterPressed = true;
-//        }
-//    }
-	
-	 private class RadioButtonHandler implements ItemListener{
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-								
-				for (int i = 0; i < pangramaButton.length; i++) {
-					if(pangramaButton[i].isSelected()) {
-						selectedPan = pangrama[i];
-					}
-					
+	public static void main(String args[]){
+		Frame f = new Frame();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(1000, 1000);
+		f.setVisible(true);
+	}
+
+	private class keyboardListener implements KeyListener{
+
+	public void keyTyped(KeyEvent e){
+
+	}
+
+	public void keyPressed(KeyEvent e){
+		System.out.println(e.getKeyChar()+" "+e.getKeyCode()+" "+KeyEvent.getKeyText(e.getKeyCode()));
+
+		panelTeclas.changeBackground(e.getKeyCode());
+
+	}
+
+	public void keyReleased(KeyEvent e){
+		panelTeclas.changeBack(e.getKeyCode());
+	}
+	}
+	private class RadioButtonHandler implements ItemListener{
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+							
+			for (int i = 0; i < pangramaButton.length; i++) {
+				if(pangramaButton[i].isSelected()) {
+					selectedPan = pangrama[i];
 				}
-
+				
 			}
-	}
+
+		}
+
+}
+
 }
